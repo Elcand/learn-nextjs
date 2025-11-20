8e9fc6bbf52bf6299a2f0444f3a17cab1c20e6e8
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-20 09:57:27 +0700

    implement link component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Link from "next/link";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,9 +9,26 @@ export default function Layout({ children }: LayoutProps) {
     <html lang="en">
       <head>
         <title>Learn Next.js</title>
-      </head>{" "}
+      </head>
       <body>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
         <footer>[footer]</footer>
       </body>
