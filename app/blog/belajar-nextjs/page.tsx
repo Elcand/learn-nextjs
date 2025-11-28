bdd3cf4000cdbd1b1bae40676e804a05acb52467
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-28 13:45:15 +0700

    using static assets

--- app/blog/belajar-nextjs/page.tsx
+++ app/blog/belajar-nextjs/page.tsx
@@ -4,6 +4,13 @@ export default function BelajarNextjs() {
   return (
     <>
       <Heading>Belajar Next.js</Heading>
+      <img
+        src="/assets/image1.jpg"
+        alt=""
+        width={360}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>haloooo</p>
     </>
   );
--- app/blog/latihan-nextjs/page.tsx
+++ app/blog/latihan-nextjs/page.tsx
@@ -4,6 +4,13 @@ export default function LatihanNextjs() {
   return (
     <>
       <Heading>Latihan Next.js</Heading>
+      <img
+        src="/assets/image2.jpg"
+        alt=""
+        width={360}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>haloooo</p>
     </>
   );
