1556caacb6d97a8c76cf710349a5e5b597b44ab0
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-28 14:16:29 +0700

    make card post list

---
+++ app/blog/layout.tsx
@@ -0,0 +1,12 @@
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <div className="w-1/4 p-4 bg-gray-200">Sidebar</div>
+      <div className="px-4">{children}</div>
+    </div>
+  );
+}
