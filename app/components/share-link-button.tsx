6c724a46459342644988435ac973520de715b133
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-29 15:36:38 +0700

    set up heroicon n add icon

--- app/components/share-link-button.tsx
+++ app/components/share-link-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { LinkIcon } from "@heroicons/react/20/solid";
+import { LinkIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -16,7 +16,7 @@ export default function ShareLinkButton() {
       onClick={handleClick}
       className="flex items-center gap-1 px-2 py-1 text-sm text-gray-500 border rounded hover:bg-gray-200 hover:text-gray-700"
     >
-      {/* <LinkIcon className="w-4 h-4" />   */}
+      <LinkIcon className="w-4 h-4" />
       {copied ? "Link Copied" : "Share Link"}
     </button>
   );
