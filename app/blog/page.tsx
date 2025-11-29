a27051d62398dd9eb1db94c2022386ff32d9d1ae
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-29 13:20:59 +0700

    make function to get list content post

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,15 @@
 import Heading from "@/app/components/heading";
 import PostCard from "../components/post-card";
+import { getAllPost } from "@/lib/post";
 
-export default function BlogPage() {
+export default async function BlogPage() {
+  const post = await getAllPost();
+
+  console.log(post);
   return (
     <>
       <Heading>Blog</Heading>
-      <h2 className="mb-3 text-2xl">ist blog</h2>
+      <h2 className="mb-3 text-2xl">its blog</h2>
       <PostCard
         title="Belajar Next.js"
         href="/blog/belajar-nextjs"
--- lib/post.ts
+++ lib/post.ts
@@ -1,7 +1,7 @@
 
 import matter from "gray-matter";
 import { marked } from "marked";
-import { readFile } from "node:fs/promises";
+import { readFile, readdir } from "node:fs/promises";
 
 export async function getPost(slug: string) {
   const text = await readFile(`./content/blog/${slug}.md`, "utf8");
@@ -12,4 +12,18 @@ export async function getPost(slug: string) {
   const body = marked(content);
 
   return { title, image, date, author, body };
+}
+
+export async function getAllPost() {
+  const files = await readdir(`./content/blog`);
+  const slugs = files.filter((file) => file.endsWith(".md")).map((file) => file.slice(0, -".md".length));
+
+  const posts =[];
+
+  for (const slug of slugs) {
+    const post = await getPost(slug);
+    posts.push(post);
+  }
+
+  return posts;
 }
